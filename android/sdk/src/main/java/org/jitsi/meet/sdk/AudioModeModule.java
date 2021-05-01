034902b71fbe6464b40a42c025a41917e27e0efd
Trex <trex@phpfox.com>
2021-05-01 21:15:28 +0700

    Improve default audio route for Android

--- android/sdk/src/main/java/org/jitsi/meet/sdk/AudioModeModule.java
+++ android/sdk/src/main/java/org/jitsi/meet/sdk/AudioModeModule.java
@@ -355,6 +355,8 @@ class AudioModeModule extends ReactContextBaseJavaModule {
             audioDevice = DEVICE_BLUETOOTH;
         } else if (headsetAvailable) {
             audioDevice = DEVICE_HEADPHONES;
+        } else if (mode == AUDIO_CALL) {
+            audioDevice = DEVICE_EARPIECE;
         } else {
             audioDevice = DEVICE_SPEAKER;
         }
